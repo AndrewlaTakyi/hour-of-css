26760fee1bf72d34529da30a792aae77ea75736c
AndrewlaTakyi <andrewla.takyi@acity.edu.gh>
2019-01-26 11:49:45 +0000

    Added javascript

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = function (e) {
+  alert("Today is the first day of the rest of your life");
+
+  var andrewla = document.getElementById("developer");
+  andrewla.innerText = "Andrewla Takyi";
+  console.log(andrewla.innerText);
+
+};
